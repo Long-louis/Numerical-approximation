e533149feb8b8611d57035700fa91af8a070862d
Long <2575090023@qq.com>
2022-10-30 14:03:38 +0800

    project4 report over

--- code/p4/first/1_2_3.py
+++ code/p4/first/1_2_3.py
@@ -1,11 +1,7 @@
 from one import *
 from scipy.optimize import curve_fit
-
-
 def func(x, a, b, c, d):
     return a + b * x + c * pow(x, 2) + d * pow(x, 3)
-
-
 if __name__ == '__main__':
     k = np.arange(0, 10, 1)
     deltadelta = []
@@ -17,15 +13,10 @@ if __name__ == '__main__':
         x = np.linspace(-1, 1, 100)
         delta = delta_f(x, cofficient, ki)
         deltadelta.append(max(delta))
-
     # 非线性最小二乘法拟合
     popt, pcov = curve_fit(func, k, deltadelta)
     # 获取popt里面是拟合系数
-    a = popt[0]
-    b = popt[1]
-    c = popt[2]
-    d = popt[3]
+    a,b,c,d = popt[0],popt[1],popt[2],popt[3]
     yvals = func(k, a, b, c, d)  # 拟合y值
-
     plot = plt.plot(k, yvals, 'r', label='拟合曲线')
     plt.show()
